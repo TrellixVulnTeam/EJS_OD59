d4232fa231b4c6f7384ca09f7ec32413d2daae04
Daniel <d13751200344@gmail.com>
2022-04-18 16:12:20 +0800

    array in ejs

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const ejs = require("ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+	//database => an array of objects
+	const languages = [
+		{ name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+		{ name: "Java", rating: 8.6, popularity: 8.2, trending: "same" },
+		{ name: "C++", rating: 6.6, popularity: 7.7, trending: "same" },
+		{ name: "PHP", rating: 2.5, popularity: 4.7, trending: "decreasing" },
+		{ name: "JavaScript", rating: 8.5, popularity: 8.1, trending: "same" },
+	];
+    res.render("index.ejs", {languages });
 });
 
 /*
@@ -20,7 +28,7 @@ app.get("/response", (req, res) => {
 	//上方可以看到使用者所輸入的資料，如{fullname: Daniel  age:18}
 	res.render("respond.ejs", {fullname, age});
 	//上方將『respond.ejs』的內容呈現在『/response』中，
-	//也讓{fullname, age}可在『respond.ejs』中使用
+	//也讓{languages}可在『respond.ejs』中使用
 });
 
 app.listen(3000, () => {
